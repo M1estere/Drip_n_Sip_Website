e7951e8fe657e0b9b396a7836e39c7559aa09fa3
Ilya_Solov <7156643@gmail.com>
2023-12-05 18:44:53 +0300

    Account page finsihed

--- account_page.php
+++ account_page.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="css/account-page.css">
+    <script src="js/account_page.js" defer></script>
 </head>
 
 <body>
@@ -17,7 +18,7 @@
         <div class="main-info">
             <div class="top-bar">
                 <div class="top-text">
-                    <span>Your Cart</span>
+                    <span>Account Information</span>
                 </div>
 
                 <div class="sign-out-button">
@@ -35,14 +36,169 @@
                     </div>
 
                     <div class="bottom">
-                        <span>Personal Information</span>
-                        <span>Billing Information</span>
-                        <span>Order History</span>
+                        <span id="pers-info-btn" onclick="enablePersInfoFunc()">Personal Information</span>
+                        <span id="bil-info-btn" onclick="enableBillingInfoFunc()">Billing Information</span>
+                        <span id="hist-info-btn" onclick="enableHistInfoFunc()">Order History</span>
                     </div>
                 </div>
 
-                <div class="right-panel">
+                <div id="personal-info" class="right-panel">
+                    <div class="common-info">
+                        <span class="title">Personal Information</span>
+                        <span class="small">Manage your personal information, including name, email and your username</span>
+                    </div>
 
+                    <div class="blocks-info">
+                        <div class="text-block">
+                            <div class="left">
+                                <span class="bold">Username</span>
+                                <span class="grey">M1estere</span>
+                            </div>
+
+                            <div class="right">
+                                <div class="circle"></div>
+                            </div>
+                        </div>
+
+                        <div class="text-block">
+                            <div class="left">
+                                <span class="bold">Name</span>
+                                <span class="grey">Ilya</span>
+                            </div>
+
+                            <div class="right">
+                                <div class="circle"></div>
+                            </div>
+                        </div>
+
+                        <div class="text-block">
+                            <div class="left">
+                                <span class="bold">Email</span>
+                                <span class="grey">7156643@gmail.com</span>
+                            </div>
+
+                            <div class="right">
+                                <div class="circle"></div>
+                            </div>
+                        </div>
+
+                        <div class="text-block">
+                            <div class="left">
+                                <span class="bold">Country</span>
+                                <span class="grey">Russia</span>
+                            </div>
+
+                            <div class="right">
+                                <div class="circle"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="billing-info" class="right-panel">
+                    <div class="common-info">
+                        <span class="title">Billing Information</span>
+                        <span class="small">Manage your personal information, including name, email and your username</span>
+                    </div>
+
+                    <div class="blocks-info">
+                        <div class="text-block">
+                            <div class="left">
+                                <span class="bold">Username</span>
+                                <span class="grey">M1estere</span>
+                            </div>
+
+                            <div class="right">
+                                <div class="circle"></div>
+                            </div>
+                        </div>
+
+                        <div class="text-block">
+                            <div class="left">
+                                <span class="bold">Name</span>
+                                <span class="grey">Ilya</span>
+                            </div>
+
+                            <div class="right">
+                                <div class="circle"></div>
+                            </div>
+                        </div>
+
+                        <div class="text-block">
+                            <div class="left">
+                                <span class="bold">Email</span>
+                                <span class="grey">7156643@gmail.com</span>
+                            </div>
+
+                            <div class="right">
+                                <div class="circle"></div>
+                            </div>
+                        </div>
+
+                        <div class="text-block">
+                            <div class="left">
+                                <span class="bold">Country</span>
+                                <span class="grey">Russia</span>
+                            </div>
+
+                            <div class="right">
+                                <div class="circle"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="order-history-info" class="right-panel">
+                    <div class="common-info">
+                        <span class="title">Order History</span>
+                        <span class="small">Manage your personal information, including name, email and your username</span>
+                    </div>
+
+                    <div class="blocks-info">
+                        <div class="text-block">
+                            <div class="left">
+                                <span class="bold">Username</span>
+                                <span class="grey">M1estere</span>
+                            </div>
+
+                            <div class="right">
+                                <div class="circle"></div>
+                            </div>
+                        </div>
+
+                        <div class="text-block">
+                            <div class="left">
+                                <span class="bold">Name</span>
+                                <span class="grey">Ilya</span>
+                            </div>
+
+                            <div class="right">
+                                <div class="circle"></div>
+                            </div>
+                        </div>
+
+                        <div class="text-block">
+                            <div class="left">
+                                <span class="bold">Email</span>
+                                <span class="grey">7156643@gmail.com</span>
+                            </div>
+
+                            <div class="right">
+                                <div class="circle"></div>
+                            </div>
+                        </div>
+
+                        <div class="text-block">
+                            <div class="left">
+                                <span class="bold">Country</span>
+                                <span class="grey">Russia</span>
+                            </div>
+
+                            <div class="right">
+                                <div class="circle"></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ js/account_page.js
@@ -0,0 +1,61 @@
+const persInfoBlock = document.getElementById("personal-info")
+const billingInfoBlock = document.getElementById("billing-info")
+const histInfoBlock = document.getElementById("order-history-info")
+
+const persInfoBtn = document.getElementById("pers-info-btn")
+const bilInfoBtn = document.getElementById("bil-info-btn")
+const histInfoBtn = document.getElementById("hist-info-btn")
+
+enablePersInfoFunc()
+
+function enablePersInfoFunc() {
+    controlBlock(persInfoBlock, true)
+    controlBlock(billingInfoBlock, false)
+    controlBlock(histInfoBlock, false)
+
+    controlButton(persInfoBtn, true)
+    controlButton(bilInfoBtn, false)
+    controlButton(histInfoBtn, false)
+}
+
+function enableBillingInfoFunc() {
+    controlBlock(persInfoBlock, false)
+    controlBlock(billingInfoBlock, true)
+    controlBlock(histInfoBlock, false)
+
+    controlButton(persInfoBtn, false)
+    controlButton(bilInfoBtn, true)
+    controlButton(histInfoBtn, false)
+}
+
+function enableHistInfoFunc() {
+    controlBlock(persInfoBlock, false)
+    controlBlock(billingInfoBlock, false)
+    controlBlock(histInfoBlock, true)
+
+    controlButton(persInfoBtn, false)
+    controlButton(bilInfoBtn, false)
+    controlButton(histInfoBtn, true)
+}
+
+function controlBlock(block, state) {
+    if (state) {
+        block.style.opacity = 1
+        block.style.position = "relative"
+        block.style.zIndex = 1
+    } else {
+        block.style.opacity = 0
+        block.style.position = "absolute"
+        block.style.zIndex = -3
+    }
+}
+
+function controlButton(button, state) {
+    if (state) {
+        button.style.transform = "translateX(5px)"
+        button.style.color = "#e2aa6a"
+    } else {
+        button.style.transform = null
+        button.style.color = null
+    }
+}
